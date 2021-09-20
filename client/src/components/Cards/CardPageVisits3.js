ee244672a8d2b318d0c6c889cebcadc35265a2c7
sdinishiya <55315619+sdinishiya@users.noreply.github.com>
2021-09-21 02:53:11 +0530

    minor changes

---
+++ client/src/components/Cards/CardPageVisits3.js
@@ -0,0 +1,108 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import props from 'prop-types';
+
+// components
+import AdminNavbar from "components/Navbars/AdminNavbar.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+import NoticeHeader from "components/Notice/NoticeHeader.js";
+import FooterAdmin from "components/Footers/FooterAdmin.js";
+
+// components
+
+export default function CardPageVisits2() {
+  
+  const [notices,setnotices]=useState([])
+
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/noticeview").then((response)=>{
+        setnotices(response.data)
+    })
+  },[])
+  return (
+    <>
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3 className="font-semibold text-base text-blueGray-700">
+                Projects On going
+              </h3>
+            </div>
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+            <Link to="/noticeView">
+                <button className="bg-emerald-400 text-white active:bg-emerald-400 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                      type="submit" >
+                      View 
+                </button> <br /><br />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent border-collapse">
+                        <thead>
+                          <tr>
+                          <th className={ "px-6 align-middle border " + "bg-blueGray-50 text-blueGray-500 border-blueGray-100"}>
+                              Name   
+                          </th>
+                          {/* <th className={ "px-6 align-middle border " + "bg-blueGray-50 text-blueGray-500 border-blueGray-100"}>
+                              Description  
+                          </th> */}
+                          <th className={ "px-6 align-middle border " + "bg-blueGray-50 text-blueGray-500 border-blueGray-100"}>
+                              Date Started  
+                          </th>
+                          <th className={ "px-6 align-middle border " + "bg-blueGray-50 text-blueGray-500 border-blueGray-100"}>
+                              End planned  
+                          </th>
+                          
+                          </tr>
+                        </thead>
+                        <tbody>
+                        {notices.map((notice)=>{
+                            const dt = new Date(notice.uploadDate);
+                            const year = dt.getFullYear() + '/';
+                            const month = ('0' + (dt.getMonth() + 1)).slice(-2) + '/';
+                            const day = ('0' + dt.getDate()).slice(-2);
+
+                            const dp = new Date(notice.expDate);
+                            const year1 = dp.getFullYear() + '/';
+                            const month1 = ('0' + (dp.getMonth() + 1)).slice(-2) + '/';
+                            const day1 = ('0' + dp.getDate()).slice(-2);
+
+
+                            return(
+                              <tr>
+                              <th className ="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                                <span className ={ "ml-3 font-bold " + "text-blueGray-600"}>
+                                  {notice.topic}
+                                </span>
+                              </th>
+                              {/* <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                  {notice.description}
+                              </td> */}
+                              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                  {year + month + day}
+                              </td>
+                              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                  {year1 + month1 + day1}
+                              </td>
+                              
+                              
+                              
+                            </tr>
+                            )
+                            })}
+                        </tbody>
+                      </table>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/components/Projects/Users/UserHeader.js
+++ client/src/components/Projects/Users/UserHeader.js
@@ -41,7 +41,7 @@ export default function ProjectUserHeader() {
             <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
               <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
                 <div className="flex-auto p-4">
-                <Link to="UserPresentProjects">
+                <Link to="UserPresentProject">
                     <div className="flex flex-wrap">
                         <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
                             <h5 className="text-blueGray-400 uppercase font-bold text-xs">
